7147867ca946b63924c84cfb0b9277fdb24a8e6b
indahmelanie <indahmelanie7@gmail.com>
2026-02-01 00:43:40 +0800

    Blade PDF done (tanda terima sertifikat, daftar hadir asesi dan penyelenggara, daftar penerimaan

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -9,24 +9,24 @@ use Illuminate\Support\Facades\DB;
 
 class PDFController extends Controller
 {
-    // public function daftarHadir()
-    // {
-    //     return Pdf::loadView('admin-panel.pdf.daftar-hadir')
-    //         ->setPaper('A4', 'portrait')
-    //         ->stream('Daftar-Hadir.pdf');
-    // }
+    public function daftarHadir()
+    {
+        return Pdf::loadView('admin-panel.pdf.daftar-hadir')
+            ->setPaper('A4', 'portrait')
+            ->stream('Daftar-Hadir.pdf');
+    }
 
-    // public function daftarPenerimaan()
-    // {
-    //     return Pdf::loadView('admin-panel.pdf.daftar-penerimaan')
-    //         ->setPaper('A4', 'landscape')
-    //         ->stream('Daftar-Penerimaan.pdf');
-    // }
+    public function daftarPenerimaan()
+    {
+        return Pdf::loadView('admin-panel.pdf.daftar-penerimaan')
+            ->setPaper('A4', 'landscape')
+            ->stream('Daftar-Penerimaan.pdf');
+    }
 
-    // public function tandaTerimaSertifikat()
-    // {
-    //     return Pdf::loadView('admin-panel.pdf.tanda-terima-sertifikat')
-    //         ->setPaper('A4', 'portrait')
-    //         ->stream('Tanda-Terima-Sertifikat.pdf');
-    // }
+    public function tandaTerimaSertifikat()
+    {
+        return Pdf::loadView('admin-panel.pdf.tanda-terima-sertifikat')
+            ->setPaper('A4', 'portrait')
+            ->stream('Tanda-Terima-Sertifikat.pdf');
+    }
 }
