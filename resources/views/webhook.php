3f20f4ec17d71fa92914a12f6751130b3aab5f28
erik-cahya <erik.cahya841@gmail.com>
2026-02-19 03:28:48 +0800

    update webhook

--- resources/views/webhook.php
+++ resources/views/webhook.php
@@ -1,18 +1,1 @@
 <?php
-
-$secret = "ISI_SECRET_GITHUB";
-
-$payload = file_get_contents('php://input');
-$signature = $_SERVER['HTTP_X_HUB_SIGNATURE_256'] ?? '';
-
-$hash = 'sha256=' . hash_hmac('sha256', $payload, $secret);
-
-if (!hash_equals($hash, $signature)) {
-    http_response_code(403);
-    exit('Invalid signature');
-}
-
-// pull repo
-shell_exec('cd /pemkab.satuproject.web.id/sertifikasi-pemkab-badung/ && git pull origin main 2>&1');
-
-echo "Deploy success";
